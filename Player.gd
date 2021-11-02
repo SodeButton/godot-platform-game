868f673f67a83e12a58448c062943c3324051b02
SodeButton <takuya.20020824@gmail.com>
2021-11-02 09:02:46 +0900

    update

---
+++ Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var speed = 200
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	
+	position.y += 100 * delta
+	position.x += velocity.x * speed * delta
